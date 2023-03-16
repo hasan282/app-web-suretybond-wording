489178afc25de91411c36e4df85b9632da46ac65
Hasan Abdullah <hsn.abdullah282@gmail.com>
2023-03-16 10:54:00 +0700

    finish dark login

--- public/asset/js/all/surety.js
+++ public/asset/js/all/surety.js
@@ -5,10 +5,12 @@ $(function () {
         if (DARK) {
             $('body').addClass('dark-mode');
             $('.main-header.navbar').removeClass('navbar-white navbar-light').addClass('navbar-dark');
+            $('.surety-logo').attr('src', '/image/icon/jis_suretybond_dark.png');
             setCookie('DRKMOD', 1, 7);
         } else {
             $('body').removeClass('dark-mode');
             $('.main-header.navbar').removeClass('navbar-dark').addClass('navbar-white navbar-light');
+            $('.surety-logo').attr('src', '/image/icon/jis_suretybond.png');
             setCookie('DRKMOD', 0, 1);
         }
     });
