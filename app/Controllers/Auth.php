d67679aa1633cf4e420824651ba0b61e56fda551
Hasan Abdullah <hsn.abdullah282@gmail.com>
2023-10-03 10:11:41 +0700

    update login ui message

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -32,9 +32,10 @@ class Auth extends BaseController
         $username = (string) $this->request->getPost('in_user');
         $password = (string) $this->request->getPost('in_pass');
         $requesturi = (string) $this->request->getPost('requested_url');
+        $redirect = redirect()->to('' . $requesturi);
         $model = new \App\Models\UserModel;
         $data = $model->getData([
-            'id', 'user', 'pass', 'nama', 'office_id',
+            'id', 'enkrip', 'user', 'pass', 'nama', 'office_id',
             'role_id', 'role', 'image', 'image_path'
         ])->where(
             ['user' => $username, 'active' => 1]
@@ -42,6 +43,8 @@ class Auth extends BaseController
         if ($data === null) {
             // false username
             $this->session->setFlashdata('login_status', 'failed');
+            $this->session->setFlashdata('fail_message', 'Username Tidak Terdaftar');
+            $this->session->setFlashdata('is_invalid', 'user');
         } else {
             $userdata = array(
                 'id' => $data['id'],
@@ -55,11 +58,15 @@ class Auth extends BaseController
             );
             if ($data['pass'] === sha3hash($password, 40)) {
                 set_userdata($userdata);
+                $redirect->setCookie('USRLOG', $data['enkrip'], 432000);
             } else {
                 // false password
                 $this->session->setFlashdata('login_status', 'failed');
+                $this->session->setFlashdata('fail_message', 'Password Tidak Sesuai');
+                $this->session->setFlashdata('username', $data['user']);
+                $this->session->setFlashdata('is_invalid', 'password');
             }
         }
-        return redirect()->to('' . $requesturi);
+        return $redirect;
     }
 }
