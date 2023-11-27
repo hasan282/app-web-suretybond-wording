f24b6a8ac467dfb4f112eb33b016f57a1120f312
Hasan Abdullah <hsn.abdullah282@gmail.com>
2023-11-27 11:49:35 +0700

    prepare

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -9,7 +9,13 @@ class Login extends BaseController
         if (is_login()) {
             return redirect()->to('dashboard');
         } else {
+            $flashdata = $this->session->getFlashdata();
             $data['userid'] = get_cookie('USRLOG');
+            $data['flash'] = array(
+                'login_fail' => ($flashdata['login_status'] ?? '') == 'failed',
+                'username' => $flashdata['username'] ?? null,
+                'invalid' => $flashdata['is_invalid'] ?? null
+            );
             $this->plugin->setup('icheck');
             return $this->view('layout/login', $data, true);
         }
--- app/Views/layout/login.php
+++ app/Views/layout/login.php
@@ -7,8 +7,6 @@ $session = session();
 $loginFail = ($session->getFlashdata('login_status') == 'failed');
 $userName = $session->getFlashdata('username');
 $invalidInput = $session->getFlashdata('is_invalid');
-$dark = get_cookie('DRKMOD') ?? '0';
-$darkmode = (intval($dark) === 1);
 ?>
 <a href="/" class="link-transparent">
     <div class="mx-auto mb-3" style="max-width:360px">
@@ -27,15 +25,26 @@ $darkmode = (intval($dark) === 1);
         <?php endif; ?>
         <form method="POST">
             <?= csrf_field(); ?>
+
+
             <input type="hidden" name="requested_url" value="<?= $session->getFlashdata('requested_url'); ?>">
-            <div class="input-group mb-3">
-                <input type="text" name="in_user" id="in_user" class="form-control<?= ($invalidInput == 'user') ? ' is-invalid' : ''; ?>" placeholder="Username" value="<?= $userName; ?>">
-                <div class="input-group-append">
-                    <div class="input-group-text">
-                        <span class="fas fa-fw fa-user"></span>
-                    </div>
-                </div>
+
+            <!-- 
+              <div class="input-group mb-3">
+                  <input type="text" name="in_user" id="in_user" class="form-control<?= ($invalidInput == 'user') ? ' is-invalid' : ''; ?>" placeholder="Username" value="<?= $userName; ?>">
+                  <div class="input-group-append">
+                      <div class="input-group-text">
+                          <span class="fas fa-fw fa-user"></span>
+                      </div>
+                  </div>
+              </div>
+           -->
+
+
+            <div class="text-center mw-2 mx-auto">
+                <img class="profile-user-img img-fluid img-circle w-100" src="/image/user/USER000M.jpg" alt="">
             </div>
+
             <div class="input-group mb-3">
                 <input type="password" name="in_pass" id="in_pass" class="form-control<?= ($invalidInput == 'password') ? ' is-invalid' : ''; ?>" placeholder="Password">
                 <div class="input-group-append">
@@ -44,11 +53,17 @@ $darkmode = (intval($dark) === 1);
                     </div>
                 </div>
             </div>
+
+
+
             <div class="text-center mt-5">
                 <button type="submit" class="btn btn-primary btn-block text-bold" disabled>
                     <i class="fas fa-sign-in-alt mr-2"></i>Login
                 </button>
             </div>
+
+
+
         </form>
     </div>
 </div>
